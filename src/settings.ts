c1662f78350a71eb9e6305661846ea86fab2b581
Donavan Becker <beckersmarthome@icloud.com>
2025-01-25 17:50:54 -0600

    Added option to use iRobot credentials to get devices info.

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,5 @@
+import type { RobotMission } from 'dorita980'
+import type { PlatformConfig } from 'homebridge'
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
@@ -7,3 +9,35 @@ export const PLATFORM_NAME = 'Roomba'
  * This must match the name of your plugin as defined the package.json
  */
 export const PLUGIN_NAME = '@homebridge-plugins/homebridge-roomba'
+
+import type { Robot } from './roomba.js'
+
+export interface RoombaPlatformConfig extends PlatformConfig {
+    devices: DeviceConfig[]
+    disableDiscovery?: boolean
+    debug?: boolean
+}
+
+export interface DeviceConfig extends Robot {
+    name: string
+    model: string
+    serialnum?: string
+    blid: string
+    robotpwd: string
+    ipaddress: string
+    cleanBehaviour: 'everywhere' | 'rooms'
+    mission?: RobotMission
+    stopBehaviour: 'home' | 'pause'
+    /**
+     * Idle Poll Interval (minutes).
+     * How often to poll Roomba's status when it is idle.
+     */
+    idleWatchInterval: number
+
+    dockContactSensor?: boolean
+    runningContactSensor?: boolean
+    binContactSensor?: boolean
+    dockingContactSensor?: boolean
+    homeSwitch?: boolean
+    tankContactSensor?: boolean
+}
